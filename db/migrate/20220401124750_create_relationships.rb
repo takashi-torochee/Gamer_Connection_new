6e4d50e932b4611eff9e4f87d62eb88ed947d1c2
EC2 Default User <ec2-user@ip-172-31-22-8.ec2.internal>
2022-04-25 12:19:12 +0000

    fixed migrate for relationships

--- db/migrate/20220401124750_create_relationships.rb
+++ db/migrate/20220401124750_create_relationships.rb
@@ -1,8 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :relationships do |t|
-      t.references :follow_id, foreign_key: { to_table: :users }
-      t.references :follower_id, foreign_key: { to_table: :users }
+      t.references :follow, foreign_key: { to_table: :users }
+      t.references :follower, foreign_key: { to_table: :users }
 
       t.timestamps
     end
